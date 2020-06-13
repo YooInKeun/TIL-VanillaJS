4f7e299e9a3b776134ed70155dc1954c87a10c8e
YooInKeun <keun0390@naver.com>
2020-06-14 00:00:36 +0900

    [UPDATE] formatting

--- ECMAScript/method.js
+++ ECMAScript/method.js
@@ -1,6 +1,11 @@
-// Map
-
 nums = [1, 2, 3, 4, 5];
+
+// Filter
+const filtered = nums.filter((item) => item > 3);
+
+console.log(filtered);
+
+// Map
 const mapped = nums.map((item) => {
   if (item > 3) {
     return item;
@@ -8,8 +13,3 @@ const mapped = nums.map((item) => {
 });
 
 console.log(mapped);
-
-// Filter
-const filtered = nums.filter((item) => item > 3);
-
-console.log(filtered);
